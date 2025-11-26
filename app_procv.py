f72f59d95b8106b69fe6e09648af96c3d0888fcf
Bruno Oliveira <brunoworkn@gmail.com>
2025-11-26 17:30:29 -0300

    funções novas

--- app_procv.py
+++ app_procv.py
@@ -33,3 +33,51 @@ def carregar_arquivo(upload_file):
             file_name=f"{nome}.xlsx",
             mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
         )
+    #interações app
+
+    st.set_page_config(page_title="Verificador de Divergência", layout="wide")
+    st.title("Verificador de Divergência entre duas bases")
+
+
+    #etapa 1: upload
+
+    st.header("1. Envie as Bases")
+    col1, col2 = st.columns(2)
+
+    with col1:
+        file1 = st.file_uploader("Base Principal (Arquivo 1)", type=['csv', 'xls', 'xlsx'])
+    with col2:
+        file2 = st.file_uploader("Base de Referência (Arquivo 2)", type=['csv', 'xls', 'xlsx'])
+    
+    if file1 and file2:
+        df1 = carregar_arquivo(file1)
+        df2 = carregar_arquivo(file2)
+
+        if df1 is not None and df2 is not None:
+            st.header("2. Escolha a Coluna de Comparação")
+            coluna1 = st.selectbox("Coluna Base (Arquivo 1)", df1.columns)
+            coluna2 = st.selectbox("Coluna Base (Arquivo 2)", df2.columns)
+
+    #etapa 3
+
+        if st.button('Executar Comparação'):
+            set_ref = set(df2[coluna2])
+            df_iguais = df1[df1[colun1].isin(set_ref)].copy()
+            df_diferentes = df1[~df1[coluna1].isin(set_ref)].copy()
+
+    # Resultados
+            st.header("3. Resultados da Comparação")
+            st.success(f" Registros Encontrados na referência: {len(df_iguais)}")
+            st.warning(f" Registros Não Encontrados na referência: {len(df_diferentes)}")
+
+            st.subheader("Registros Encontrados")
+            st.dataframe(df_iguais, use_container_width=True)
+            gerar_download(df_iguais, "registros_iguais")
+
+            st.subheader("Registros Não Encontrados")
+            st.dataframe(df_diferentes, use_container_width=True)
+            gerar_download(df_diferentes, "registros_diferentes")
+        else:
+            st.error("Erro ao processar os arquivos.")
+    else:
+        st.info("Envie os dois arquivos para começar a comparação.")
